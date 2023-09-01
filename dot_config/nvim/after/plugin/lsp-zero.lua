167fe03280eed9717ab66bad6b4125609a8c63ae
Lerskk <faustoleo02@gmail.com>
2023-09-01 17:47:03 -0300

    setup cmp

--- dot_config/nvim/after/plugin/lsp-zero.lua
+++ dot_config/nvim/after/plugin/lsp-zero.lua
@@ -1,6 +1,4 @@
-local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
+local lsp = require('lsp-zero').preset({ name = 'recommended' })
 
 lsp.ensure_installed {
   'html',
@@ -12,26 +10,69 @@ lsp.ensure_installed {
   'tailwindcss',
 }
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings {
-  ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space'] = cmp.mapping.complete()
-}
+lsp.on_attach(function(_, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
 
-lsp.setup_nvim_cmp {
-  mapping = cmp_mappings
-}
+  local opts = { buffer = bufnr }
 
-lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
-
-  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+lsp.format_on_save {
+  servers = {
+    ['lua_ls'] = { 'lua' },
+    ['tsserver'] = { 'typescript' },
+  }
+}
+
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local lspkind = require('lspkind')
+
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format {
+      mode = 'symblo_text',
+      maxwidth = 50,
+      ellipsis_char = '...',
+    }
+  },
+  mapping = lsp.defaults.cmp_mappings({
+    ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(cmp_select), { 'i', 'c' }),
+    ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(cmp_select), { 'i', 'c' }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.confirm(), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping(cmp.mapping.abort(), { 'i', 'c' }),
+  }),
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+}
+
+cmp.setup.cmdline({ '/', '?' }, {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  -- mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
--- dot_config/nvim/lua/lerskk/plugins-setup.lua
+++ dot_config/nvim/lua/lerskk/plugins-setup.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -21,21 +21,21 @@ local status, packer = pcall(require, 'packer')
 if not status then return end
 
 return packer.startup(function(use)
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
 
-	-- themes
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use 'rktjmp/lush.nvim'
-	use '~/.config/nvim/minimal-dark'
+  -- themes
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use 'rktjmp/lush.nvim'
+  use '~/.config/nvim/minimal-dark'
 
-	-- tmux
-	use 'christoomey/vim-tmux-navigator'
+  -- tmux
+  use 'christoomey/vim-tmux-navigator'
 
-  -- fzf 
+  -- fzf
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.2',
-    requires = { 
+    requires = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
     }
@@ -48,31 +48,35 @@ return packer.startup(function(use)
   }
 
   -- autocompletion
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-cmdline' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'onsails/lspkind-nvim' },
+      { 'L3MON4D3/LuaSnip' }, -- Required
+    }
   }
-}
 
   -- surrounds
   use {
     'kylechui/nvim-surround',
-    config = function() require('nvim-surround').setup{} end
+    config = function() require('nvim-surround').setup {} end
   }
+  use 'windwp/nvim-ts-autotag'
 
-  
 
-	if packer_bootstrap then
-		require('packer').sync()
-	end
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
